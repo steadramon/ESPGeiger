df811798f9e50ec3e9e2bbb416d815df72909ac1
steadramon <866195+steadramon@users.noreply.github.com>
2023-09-30 12:37:02 +0100

    Version release file

--- scripts/prepare_deploy.sh
+++ scripts/prepare_deploy.sh
@@ -3,6 +3,7 @@ set -e
 echo "renaming bin files with the environment name"
 rm .pio/build/*/bootloader.bin
 rm .pio/build/*/partitions.bin
+rename -v 's/.bin/.'"$1"'.bin/' .pio/build/*/*.bin
 rename -v 's:/:-:g' .pio/build/*/*.bin
 mkdir toDeploy
 rename 's/.pio-build-//' .*.bin
