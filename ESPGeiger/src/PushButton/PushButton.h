21f9099eb19d37da4fb84d65d7480627803bc1cf
steadramon <866195+steadramon@users.noreply.github.com>
2023-10-15 19:06:14 +0100

    Beep when enabling/disabling screen

--- ESPGeiger/src/PushButton/PushButton.h
+++ ESPGeiger/src/PushButton/PushButton.h
@@ -41,17 +41,21 @@ static void IRAM_ATTR do_longpress() {
   }
 };
 #else
-static void ICACHE_RAM_ATTR do_pushbutton() {
+static void do_pushbutton() {
   status.button_pushed = true;
 }
 
-static void ICACHE_RAM_ATTR do_longpress() {
-#ifdef ESPGEIGER_HW
-    status.blip_led.Blink(250,250);
-#endif
+static void do_longpress() {
   status.enable_oled_timeout = !status.enable_oled_timeout;
   if (status.enable_oled_timeout) {
     status.oled_timeout = 0;
+#ifdef ESPGEIGER_HW
+    status.blip_led.Blink(200,100).Repeat(2);
+#endif
+  } else {
+#ifdef ESPGEIGER_HW
+    status.blip_led.Blink(200,1).Repeat(1);
+#endif
   }
 }
 #endif
