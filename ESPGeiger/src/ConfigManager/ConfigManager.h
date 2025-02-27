ac35562c9576318128a4f3572144035760bce509
steadramon <866195+steadramon@users.noreply.github.com>
2025-02-27 20:02:24 +0000

    Avoid erasing all settings on wifi reset

--- ESPGeiger/src/ConfigManager/ConfigManager.h
+++ ESPGeiger/src/ConfigManager/ConfigManager.h
@@ -120,7 +120,7 @@ public:
   void preSaveParams();
   void saveParams();
   void delay(unsigned long m);
-  void resetSettings();
+  void eraseSettings();
   const char* getHostName() { return hostName; };
   const char* getChipID() { return chipId; };
   const char* getThingName() { return status.thingName; };
