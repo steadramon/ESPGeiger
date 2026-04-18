b94fc05ef737522ebdd51414d4a9f3d8dd8dc0bb
steadramon <866195+steadramon@users.noreply.github.com>
2026-04-18 21:14:26 +0100

    sdfat 2.3.1

--- lib/default/SdFat-2.3.1/examples/ExFatLogger/ExFatLogger.h
+++
@@ -1,7 +0,0 @@
-// Avoid IDE problems by defining struct in septate .h file.
-// Pad record so size is a power of two for best write performance.
-#pragma once
-const size_t ADC_COUNT = 4;
-struct data_t {
-  uint16_t adc[ADC_COUNT];
-};
