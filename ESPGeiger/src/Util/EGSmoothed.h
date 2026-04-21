11ad58e988f77024802895747717b3f27c0ae26c
steadramon <866195+steadramon@users.noreply.github.com>
2026-04-21 14:50:11 +0100

    Add our copyright notice

--- ESPGeiger/src/Util/EGSmoothed.h
+++ ESPGeiger/src/Util/EGSmoothed.h
@@ -14,7 +14,19 @@
     float v = avg.get();
 
   Copyright (C) 2026 @steadramon
-  GPLv3 or later - see project COPYING / license notice.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 #ifndef EGSMOOTHED_H
 #define EGSMOOTHED_H
