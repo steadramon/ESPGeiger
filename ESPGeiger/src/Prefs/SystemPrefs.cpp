1c3facbd3800e14b43db657c3de10adc02182252
steadramon <866195+steadramon@users.noreply.github.com>
2026-04-23 11:08:25 +0100

    Add pulse width configuration for test builds

--- ESPGeiger/src/Prefs/SystemPrefs.cpp
+++ ESPGeiger/src/Prefs/SystemPrefs.cpp
@@ -19,6 +19,9 @@
 #include "SystemPrefs.h"
 #include "../Module/EGModuleRegistry.h"
 #include "../GeigerInput/GeigerInput.h"
+#if GEIGER_IS_TEST(GEIGER_TYPE)
+#include "../GeigerInput/GeigerInputTest.h"
+#endif
 
 #define _STR(x) #x
 #define STR(x)  _STR(x)
@@ -96,6 +99,9 @@ static const EGPref INPUT_PREF_ITEMS[] = {
 #if GEIGER_IS_PULSE(GEIGER_TYPE) && !defined(USE_PCNT)
   {"debounce", "Debounce", "Debounce (us)", STR(GEIGER_DEBOUNCE), nullptr, 0, 10000, 0, EGP_UINT, 0},
 #endif
+#if GEIGER_TYPE == GEIGER_TYPE_TEST || GEIGER_TYPE == GEIGER_TYPE_TESTPULSE
+  {"pulse_width_us", "Pulse width", "Simulated pulse width (us)", STR(GEIGER_PULSE_WIDTH), nullptr, 10, 2000, 0, EGP_UINT, 0},
+#endif
 #ifndef DISABLE_INTERNAL_BLIP
   {"blip_led",    "Blip LED",       "Flash LED on each count", "1",   nullptr, 0, 1,   0, EGP_BOOL, 0},
 #if !(GEIGER_IS_TEST(GEIGER_TYPE) && defined(ESP8266))
@@ -131,6 +137,9 @@ void InputPrefs::on_prefs_loaded() {
 #endif
 #if GEIGER_IS_PULSE(GEIGER_TYPE) && !defined(USE_PCNT)
   gcounter.set_debounce((int)EGPrefs::getUInt("input", "debounce"));
+#endif
+#if GEIGER_TYPE == GEIGER_TYPE_TEST || GEIGER_TYPE == GEIGER_TYPE_TESTPULSE
+  _pulse_width_us = (int)EGPrefs::getUInt("input", "pulse_width_us");
 #endif
 #ifndef DISABLE_INTERNAL_BLIP
   gcounter.set_blip_led(EGPrefs::getBool("input", "blip_led"));
