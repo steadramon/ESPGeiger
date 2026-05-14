6e1ba3eba95dbd7c8998f41af30837da33062af6
steadramon <866195+steadramon@users.noreply.github.com>
2026-05-14 09:37:44 +0100

    Added gpl

--- ESPGeiger/src/WebPortal/WebPortal.h
+++ ESPGeiger/src/WebPortal/WebPortal.h
@@ -1,11 +1,20 @@
 /*
   WebPortal.h - Async web UI for ESPGeiger
 
-  Owns one EGHttpServer instance, registers cross-cutting routes (/, /info,
-  /style.css), and walks a static list of per-module registerRoutes() calls
-  at boot. Replaces the WiFiManager + ESP8266WebServer running web UI.
+  Copyright (C) 2026 @steadramon
 
-  License: GPL-3.0
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 #ifndef WEBPORTAL_H
 #define WEBPORTAL_H
